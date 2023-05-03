a86cc5462c4657340e661ebaf1e423597f9cd923
rretroo <83876182+rretroo@users.noreply.github.com>
2023-05-03 12:32:31 +0200

    Update q3a.ps1

--- q3a.ps1
+++ q3a.ps1
@@ -36,6 +36,7 @@ function Download-File {
   )
 
   Write-Host "Downloading $Path ..."
+  $ProgressPreference = 'SilentlyContinue'
   Invoke-WebRequest -Uri $Url -OutFile $Path -UseBasicParsing -TimeoutSec 600 -Method GET -ContentType "application/zip" -Headers @{"Accept-Encoding"="gzip";"User-Agent"="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
 }
 
