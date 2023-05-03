44c6f51e0638c5ae776e921832d35eb3fab5b271
rretroo <83876182+rretroo@users.noreply.github.com>
2023-05-03 19:05:51 +0200

    Update q3a.ps1

--- q3a.ps1
+++ q3a.ps1
@@ -52,13 +52,13 @@ Write-Host "====  brought to you by rretroo   ===="
 $dlPath = "$HOME\Downloads"
 $ioq3Path = "$dlPath\Quake3"
 
-if -Not (Test-Path $ioq3Path) {
+If (Test-Path $ioq3Path) { } Else
   New-Item -ItemType Directory -Path $ioq3Path
 }
 
 Download-File -Url "https://fte.triptohell.info/moodles/win32/fteqw.exe" -Path "$ioq3Path\fteqw.exe"
 
-If -Not (Test-Path "$ioq3path\baseq3") {
+If (Test-Path "$ioq3path\baseq3") { } Else
   New-Item -ItemType Directory -Path "$ioq3Path\baseq3"
 }
 
