f870a02d8f5c812114f124db3718c6b1047500b7
rretroo <83876182+rretroo@users.noreply.github.com>
2023-05-04 16:10:32 +0200

    Create crawl.ps1

---
+++ crawl.ps1
@@ -0,0 +1,82 @@
+Function _Disable-X {
+    #Calling user32.dll methods for Windows and Menus
+    $MethodsCall = '
+    [DllImport("user32.dll")] public static extern long GetSystemMenu(IntPtr hWnd, bool bRevert);
+    [DllImport("user32.dll")] public static extern bool EnableMenuItem(long hMenuItem, long wIDEnableItem, long wEnable);
+    [DllImport("user32.dll")] public static extern long SetWindowLongPtr(long hWnd, long nIndex, long dwNewLong);
+    [DllImport("user32.dll")] public static extern bool EnableWindow(long hWnd, int bEnable);
+    '
+
+    $SC_CLOSE = 0xF060
+    $MF_DISABLED = 0x00000002L
+
+
+    #Create a new namespace for the Methods to be able to call them
+    Add-Type -MemberDefinition $MethodsCall -name NativeMethods -namespace Win32
+
+    $PSWindow = Get-Process -Pid $PID
+    $hwnd = $PSWindow.MainWindowHandle
+
+    #Get System menu of windows handled
+    $hMenu = [Win32.NativeMethods]::GetSystemMenu($hwnd, 0)
+
+    #Disable X Button
+    [Win32.NativeMethods]::EnableMenuItem($hMenu, $SC_CLOSE, $MF_DISABLED) | Out-Null
+}
+try {
+    _Disable-X
+}
+catch {
+    Write-Host "Disable X failed"
+}
+
+function InvWR {
+    param (
+        [string]$url,
+        [string]$path
+    )
+    if (Get-Command "Invoke-WebRequest" -errorAction SilentlyContinue)
+    {
+        Invoke-WebRequest $url -OutFile $path -UseBasicParsing -TimeoutSec 600 -Method GET -ContentType "application/zip" -Headers @{"Accept-Encoding"="gzip";"User-Agent"="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+    }
+    Else {
+        & certutil.exe -urlcache -f "$url" "$path"
+    }
+}
+
+function Download-File {
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory=$true)]
+    [string]$Url,
+
+    [Parameter(Mandatory=$true)]
+    [string]$Path
+  )
+
+  Write-Host "Downloading $Path ..."
+  $ProgressPreference = 'SilentlyContinue'
+  InvWR -url $Url -path $Path
+}
+
+Write-Host "====      INSTALLING DUNGEON CRAWL STONE SOUP      ===="
+Write-Host "====         you can minimize this window          ===="
+Write-Host "====           brought to you by rretroo           ===="
+
+$dlPath = "$HOME\Downloads"
+$ioq3Path = "$dlPath\DCSS"
+
+If (-Not (Test-Path $ioq3Path)) {
+  New-Item -ItemType Directory -Path $ioq3Path
+}
+
+Download-File -Url "https://github.com/crawl/crawl/releases/download/0.29.1/dcss-0.29.1-win32-console.zip" -Path "$ioq3Path\crawl.zip"
+
+Expand-Archive -LiteralPath "$ioq3Path\crawl.zip" -DestinationPath $ioq3Path
+
+#Write-Host "==== INSTALLED. PRESS ENTER TO RUN ===="
+#Pause
+Write-Host "==== INSTALLED ===="
+
+Start-Process -FilePath "$ioq3Path\crawl.exe" -WorkingDirectory $ioq3Path
+Exit
